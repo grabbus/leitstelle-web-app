63a2b26674035397eee2b35a61bfa222dae2d351
grabbus <mail@michaelgrabinger.com>
2024-04-15 19:08:35 +0200

    added first livewire components

---
+++ app/Livewire/ControlCenter/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\ControlCenter;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.control-center.index');
+    }
+}
