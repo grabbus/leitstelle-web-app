d07f4854d07c385282fe41b9dc1b711feab53ccd
grabbus <mail@michaelgrabinger.com>
2024-04-15 19:41:55 +0200

    meh

---
+++ resources/views/monitor/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+    <div class="container mt-2">
+        @livewire('monitor.index')
+    </div>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layout');
 });
+Route::get('/control-center', function () {
+    return view('control-center.index');
+});
+Route::get('/monitor', function () {
+    return view('monitor.index');
+});
